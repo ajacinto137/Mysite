d7bf2ec5abaf84b45da2ac8f7d5d733d62264372
ajacinto137 <ajacinto137>
2020-04-04 21:34:58 -0400

    Changed File names

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""Blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('polls.urls')),
+    path('', include('Home.urls')),
     path('admin/', admin.site.urls),
 ]
